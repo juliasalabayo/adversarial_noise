44ad6cb2e42688655d97b1c563f6dd02c151dfcb
Júlia Sala-Bayo <juliasala-bayo@Julia-SalaBayo.local>
2024-12-08 10:47:18 +0000

    test: add tests for image preparation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
-from adversarial_noise.utils import load_model
+from adversarial_noise.utils import load_model, prepare_image
+from PIL import Image
 
 
 @pytest.mark.parametrize("model_name", ["resnet50"])
@@ -15,3 +16,17 @@ def test_load_model(model_name: str) -> None:
     assert (
         next(model.parameters()).device == device
     ), f"Model should be loaded to {device}"
+
+
+def test_preprocess_image() -> None:
+    """Test if the image preprocessing works correctly."""
+    # Create a dummy image
+    image = Image.new("RGB", (256, 256), color="red")
+    image_tensor = prepare_image(image)
+
+    # Output tensor should be 4D (batch size, channels, height, width)
+    assert image_tensor.ndimension() == 4
+    # Image should be resized to 224x224 with 3 channels
+    assert image_tensor.shape[1:] == torch.Size([3, 224, 224])
+    # Tensor should be of dtype float32
+    assert image_tensor.dtype == torch.float32
