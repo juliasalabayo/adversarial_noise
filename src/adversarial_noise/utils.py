ad110dd596c0f48027aa7f26f147906450e35607
Júlia Sala-Bayo <juliasala-bayo@Julia-SalaBayo.local>
2024-12-08 10:46:49 +0000

    feat: add image processing methods

--- src/adversarial_noise/utils.py
+++ src/adversarial_noise/utils.py
@@ -1,5 +1,8 @@
+from typing import Literal
+
 import torch
-from torchvision import models
+from PIL import Image
+from torchvision import models, transforms
 
 AVAILABLE_MODELS = {"resnet50": "ResNet50_Weights"}
 
@@ -50,3 +53,81 @@ def get_model_categories(model_name: str) -> list[str]:
     return getattr(models, AVAILABLE_MODELS[model_name]).DEFAULT.meta[
         "categories"
     ]
+
+
+def convert_category_to_tensor(
+    target_category: str, categories: list[str], device: torch.device
+) -> torch.Tensor:
+    """
+    Convert a label category to its tensor representation.
+
+    Parameters
+    ----------
+        target_category: Target classification label.
+        categories: List of available labels.
+
+    Returns
+    -------
+        torch.Tensor: Category tensor
+    """
+    if target_category not in categories:
+        msg = f"Category {target_category} not found in categories."
+        raise ValueError(msg)
+    target_tensor = torch.tensor(
+        [categories.index(target_category)], dtype=torch.long
+    )
+
+    return target_tensor.to(device)
+
+
+def prepare_image(image: Image) -> torch.Tensor:
+    """
+    Tranform an image to be interpreted by ResNet models.
+
+    It uses centercrop to preserve the aspect ratio, preferred in some models,
+    like ResNet.
+
+    Parameters
+    ----------
+        image: Image to prepare for inputting to the models.
+
+    Returns
+    -------
+        torch.Tensor: Processed image.
+    """
+    transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),
+        ]
+    )
+    # Convert image to tensor
+    input_tensor = transform(image)
+    # Add batch dimension, needed by the model
+    input_batch = input_tensor.unsqueeze(0)
+
+    # Ensure tensor is of type float32
+    return input_batch.to(torch.float32)
+
+
+def get_image(
+    image_path: str,
+    colour_scale: Literal["RGB", "Grayscale", "CYMK", "RGBA"] = "RGB",
+) -> torch.Tensor:
+    """
+    Load image from a file and process it for ResNet use.
+
+    Parameters
+    ----------
+        image_path: Path to the image file.
+
+    Returns
+    -------
+        Image: Loaded image.
+    """
+    image = Image.open(image_path).convert(colour_scale)
+    return prepare_image(image)
